386dde30047896fb5510fc69aa618f170d5edf57
Vlados <test@mai.da>
2025-06-20 21:36:49 +0700

    autorization

---
+++ admin/js/auto.js
@@ -0,0 +1,13 @@
+let login = "admin";
+let password = "adminpass";
+let alrt = document.getElementsByName("alert")[0];
+document.querySelector("button").addEventListener("click", function(){
+    let inslogin = document.getElementById("login").value;
+    let inspass = document.getElementById("pass").value;
+    if (login == inslogin && password == inspass) {
+    window.location = '/admin/index.html';
+    }
+    else{
+        alrt.style.display = "unset";
+    }
+})
