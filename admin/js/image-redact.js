3f2b6971699c67644083f7ced22eda8cef8ec0d6
Vlados <test@mai.da>
2025-06-22 12:07:32 +0700

    forms2 small fix

--- admin/js/image-redact.js
+++ admin/js/image-redact.js
@@ -12,9 +12,10 @@ var imageUploadButton = document.querySelector("#upload.image");
 var imageCloseCross= document.querySelector("#close.image");
 var imageCloseButton= document.querySelector("#cancel.image");
 var imageInput = document.querySelector("#file-upload.image");
+var imageSize= document.querySelector("#imageSize");
 imageInput.value = "";
 var imagePreview = document.querySelector("#preview.image");
-
+imagePreview.style.width= "80%"; 
 function fall (){
     modalImage.style.animation='fall';
     modalImage.style.animationDuration='1s';
@@ -32,5 +33,23 @@ function imagePrev() {
     console.log(imageInput.files[0]);
     imagePreview.src = URL.createObjectURL(imageInput.files[0]);
 }
+function changeSize() {
+    console.log(imageSize.value);
+    switch (imageSize.value) {
+        case "1/1":
+           imagePreview.style.width="80%"; 
+            break;
 
+        case "1/2":
+           imagePreview.style.width="40%"; 
+            break;
+        case "1/4":
+           imagePreview.style.width="20%"; 
+            break;
+        default:
+            break;
+    }
+    
+}
 imageInput.addEventListener("change", imagePrev);
+imageSize.addEventListener("change", changeSize);
