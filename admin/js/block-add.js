28ef226aaf51c73901c24e4af2018f7c65d5f822
taleOfTheCruelWorld <gomonov638@gmail.com>
2025-06-22 20:14:57 +0700

    адаптация

--- admin/js/block-add.js
+++ admin/js/block-add.js
@@ -4,17 +4,14 @@ var blockPref = document.getElementById("block-pref");
 
 blockPref.onclick = function () {
    modalBlock.style.display="block"; 
-   modalBlock.style.animation='rise';
-   modalBlock.style.animationDuration='1s';
+
 }
 
 var blockUploadButton = document.querySelector("#upload.block");
 var blockCloseCross= document.querySelector("#close.block");
 var blockSize= document.querySelector("#blockType");
 function blockFall (){
-    modalBlock.style.animation='fall';
-    modalBlock.style.animationDuration='1s';
-    setTimeout(() =>{modalBlock.style.display="none";}, 1000);
+   modalBlock.style.display = 'none';
 }
 blockCloseCross.onclick = blockFall;
 blockUploadButton.onclick = blockFall;
--- admin/js/table-create.js
+++ admin/js/table-create.js
@@ -4,17 +4,14 @@ var tablePref = document.getElementById("table-pref");
 
 tablePref.onclick = function () {
    modalTable.style.display="block"; 
-   modalTable.style.animation='rise';
-   modalTable.style.animationDuration='1s';
+
 }
 
 var tableUploadButton = document.querySelector("#upload.table");
 var tableCloseCross= document.querySelector("#close.table");
 var tableSize= document.querySelector("#tableType");
 function tableFall (){
-    modalTable.style.animation='fall';
-    modalTable.style.animationDuration='1s';
-    setTimeout(() =>{modalTable.style.display="none";}, 1000);
+ modalTable.style.display='none';
 }
 tableCloseCross.onclick = tableFall;
 tableUploadButton.onclick = tableFall;
